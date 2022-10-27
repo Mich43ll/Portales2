5878039f82bbad487fbefca6316ae3981274e96d
Mich43ll <mosoriobarahona@gmail.com>
2022-10-26 22:33:50 -0600

    correccion

--- Empleados.php
+++ Empleados.php
@@ -2,7 +2,115 @@
 require_once "src/vistas/meta.php";
 require_once "src/vistas/modal.php";
 require_once "src/vistas/header.php";
-require_once "src/vistas/maestros-vista.php";
+?>
+<div class="all-title-box">
+		<div class="container text-center">
+			<h1 style="color: black;">Maestros<span class="m_1">"La educación es lo que sobrevive cuando lo que se ha aprendido ya se ha olvidado."</span></h1>
+		</div>
+	</div>
+	
+	<div id="teachers" class="section wb">
+        <div class="container">
+            <div class="row">
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="our-team">
+						<div class="team-img">
+							<img src="./src/images/author.jpg">
+						</div>
+						<div class="team-content">
+							<h3 class="title">Keyti Diaz</h3>
+							<span class="post">Ciencias Sociales</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="our-team">
+						<div class="team-img">
+							<img src="./src/images/avatar-01.jpg">
+						</div>
+						<div class="team-content">
+							<h3 class="title">Ramon Ordoñez</h3>
+							<span class="post">Matematicas</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="our-team">
+						<div class="team-img">
+							<img src="./src/images/avatar-02.jpg">
+						</div>
+						<div class="team-content">
+							<h3 class="title">Maria Castillo</h3>
+							<span class="post">Ingles</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="our-team">
+						<div class="team-img">
+							<img src="./src/images/avatar-03.jpg">
+						</div>
+						<div class="team-content">
+							<h3 class="title">Johana Savillon</h3>
+							<span class="post">Español</span>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="our-team">
+						<div class="team-img">
+							<img src="./src/images/avatar-04.jpg">
+						</div>
+						<div class="team-content">
+							<h3 class="title">William mendez</h3>
+							<span class="post">Educación fisica</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="our-team">
+						<div class="team-img">
+							<img src="./src/images/avatar-05.jpg">
+						</div>
+						<div class="team-content">
+							<h3 class="title">Marcos Valladares</h3>
+							<span class="post">Musica</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="our-team">
+						<div class="team-img">
+							<img src="./src/images/avatar-06.jpg">
+						</div>
+						<div class="team-content">
+							<h3 class="title">Pedro Ramos</h3>
+							<span class="post">Computación</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="our-team">
+						<div class="team-img">
+							<img src="./src/images/avatar-07.jpg">
+						</div>
+						<div class="team-content">
+							<h3 class="title">Miranda Lopez</h3>
+							<span class="post">Ciencias Naturales</span>
+						</div>
+					</div>
+				</div>
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div><!-- end section -->	
+<?php
 require_once "src/vistas/footer.php";
 ?>
 
