dbc281b77c5dbcf001fc70e945c7a096e4db7229
Carlos0campo <Carlosocampoavila@gmail.com>
2022-10-27 15:43:38 -0600

    en vistas crree un archivo alumnos donde estara la pagina de alumnos

---
+++ src/vistas/alumnos/inicio.php
@@ -0,0 +1,20 @@
+<?php
+require_once "src/vistas/meta.php";
+require_once "src/vistas/modal.php";
+require_once "src/vistas/header.php";
+?>
+<div class="about">
+         <div class="container-fluid">
+            <div class="row">
+               <div class="col-md-6 offset-md-3">
+                  <div class="titlepage">
+                    <p>Esta es la pagina inicial del modulo de alumnos</p>                   
+                  </div>
+               </div>
+               </div>
+            </div>
+         </div>
+      </div>
+<?php
+require_once "src/vistas/footer.php";
+?>
